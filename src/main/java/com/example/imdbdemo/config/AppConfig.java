2f400a9393bcc734d9166b59bd3c6f718b4547f2
Liam Hennessy <liam-hennessy@outlook.com>
2025-07-02 00:04:43 +1200

    Improved env validation
    - Added back validation to backend, but applied it to all environment variables
    - Set default useTls to false for frontend

--- src/main/java/com/example/imdbdemo/config/AppConfig.java
+++ src/main/java/com/example/imdbdemo/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.example.imdbdemo.config;
 
+import com.example.imdbdemo.config.props.AppProps;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
+@EnableConfigurationProperties({AppProps.class})
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AppConfig {
 
---
+++ src/main/java/com/example/imdbdemo/config/props/AppProps.java
@@ -0,0 +1,35 @@
+package com.example.imdbdemo.config.props;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "imdb-demo")
+public record AppProps (
+	@NotBlank String host,
+	@Min(1024) @Max(65535) int port,
+	@NotNull boolean useTls,
+	@Valid DbProps db,
+	@Valid KsProps ks
+) {
+
+	public record DbProps (
+		@NotBlank String host,
+		@Min(1024) @Max(65535) int port,
+		@NotBlank String name,
+		@NotBlank String user,
+		@NotBlank String pass
+	) {}
+
+	public record KsProps (
+		@NotBlank String path,
+		@NotBlank String type,
+		@NotBlank String alias,
+		@NotBlank String pass
+	) {}
+}
